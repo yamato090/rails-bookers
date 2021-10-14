00a118c13de44fbf254645faba463de13a7dd264
yamato090 <yamauchi09012@gmail.com>
2021-10-14 11:39:27 +0000

    add　削除機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,11 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
     @book = Book.new
-    @book = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def create
@@ -14,6 +15,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+   def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to books_path(params[:id])
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private

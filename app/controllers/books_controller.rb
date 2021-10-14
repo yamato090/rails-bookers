a36a25641dd5662515dcb51543dfcf06b0c0ad14
yamato090 <yamauchi09012@gmail.com>
2021-10-14 13:32:15 +0000

    add フラッシュメッセージの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,18 +9,25 @@ class BooksController < ApplicationController
   end
 
   def create
+    @books = Book.all
     @book = Book.new(book_params)
-    @book.save
-    redirect_to '/books'
-  end
+    if @book.save
+      flash[:notice] = "successfully"
+      redirect_to books_path(@book)
+    else
+      flash.now[:error] = @book.errors.full_message
+      render :index
+    end
+  end  
 
   def edit
     @book = Book.find(params[:id])
   end
   
-   def update
+  def update
     @book = Book.find(params[:id])
     @book.update(book_params)
+    flash[:notice] = "successfully"
     redirect_to books_path(params[:id])
   end
   
@@ -34,5 +41,5 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+end  
 
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
 
   resources :books
   root :to => 'homes#top'
-  post 'books' => 'books#index'
-  get 'books#index' => 'books#show'
 
   
 end
